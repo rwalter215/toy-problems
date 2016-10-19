071da8cf6aeeda3918666c523adeb2bdb9c004f4
Ryan Walter <r.walter215@gmail.com>
2016-10-19 11:46:51 -0500

    added comments to bubbleSort

--- toy_problems/Misc./BubbleSort.js
+++ toy_problems/Misc./BubbleSort.js
@@ -1,11 +1,17 @@
+// sort an array using bubbleSort
+
 function bubbleSort(array) {
-  let temp;
-  let swap;
+  let temp; // used to temporarily hold a variable's value
+  let swap; // variable(boolean) used to keep track if we have made a swap this iteration
   const sortArray = array.slice();
 
   do {
-    swap = false;
+    swap = false; // each iteration we set swap to false
+
+    // while continue to make swaps, we iterate through array and see if any values are out of order
     for (let i = 0; i < sortArray.length; i++) {
+      // if 2 numbers are out of order, we swap those values,
+      // and set swap to true
       if (sortArray[i] > sortArray[i + 1]) {
         temp = sortArray[i];
         sortArray[i] = sortArray[i + 1];
@@ -14,7 +20,7 @@ function bubbleSort(array) {
       }
     }
   } while (swap === true);
-  return sortArray;
+  return sortArray; // return the sorted array
 }
 
 bubbleSort([3, 1, 9, 5, 7, 2, 4, 8, 6]);
